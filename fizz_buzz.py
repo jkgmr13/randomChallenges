caef506800f518d0b8c279dc569192a7fde93793
jkgmr13 <jokell13552@gmail.com>
2021-08-03 10:12:05 -0600

    made it run

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,14 +1,14 @@
-def fizz_buzz(num: int) -> String:
-  if num % 3 == 0 and num % 5 == 0:
-    return "FizzBuzz"
-  elif num % 3 == 0:
-    return "Fizz"
-  elif num % 5 == 0:
-    return "Buzz"
-  else:
-    return String(num)
+def fizz_buzz(num: int):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
   
-  print(fizz_buzz(6))
-  print(fizz_buzz(10))
-  print(fizz_buzz(30))
-  print(fizz_buzz(4))
+print(fizz_buzz(6))
+print(fizz_buzz(10))
+print(fizz_buzz(30))
+print(fizz_buzz(4))
